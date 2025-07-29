b72851af9663e61d95c9e01c6b059e8b25913da6
AdiDEVS1 <adityagupta.926099@gmail.com>
2025-07-29 02:55:00 -0400

    Improved test coverage for Account model

--- service/models.py
+++ service/models.py
@@ -3,6 +3,7 @@ Models for Account
 
 All of the models are stored in this module
 """
+
 import logging
 from datetime import date
 from flask_sqlalchemy import SQLAlchemy
@@ -26,30 +27,22 @@ def init_db(app):
 #  P E R S I S T E N T   B A S E   M O D E L
 ######################################################################
 class PersistentBase:
-    """Base class added persistent methods"""
-
-    def __init__(self):
-        self.id = None  # pylint: disable=invalid-name
+    """Base class with persistent methods"""
 
     def create(self):
-        """
-        Creates a Account to the database
-        """
-        logger.info("Creating %s", self.name)
-        self.id = None  # id must be none to generate next primary key
+        """Creates a record in the database"""
+        logger.info("Creating %s", str(self))
         db.session.add(self)
         db.session.commit()
 
     def update(self):
-        """
-        Updates a Account to the database
-        """
-        logger.info("Updating %s", self.name)
+        """Updates a record in the database"""
+        logger.info("Updating %s", str(self))
         db.session.commit()
 
     def delete(self):
-        """Removes a Account from the data store"""
-        logger.info("Deleting %s", self.name)
+        """Deletes a record from the database"""
+        logger.info("Deleting %s", str(self))
         db.session.delete(self)
         db.session.commit()
 
@@ -57,22 +50,20 @@ class PersistentBase:
     def init_db(cls, app):
         """Initializes the database session"""
         logger.info("Initializing database")
-        cls.app = app
-        # This is where we initialize SQLAlchemy from the Flask app
         db.init_app(app)
         app.app_context().push()
-        db.create_all()  # make our sqlalchemy tables
+        db.create_all()
 
     @classmethod
     def all(cls):
-        """Returns all of the records in the database"""
-        logger.info("Processing all records")
+        """Returns all records from the database"""
+        logger.info("Fetching all records")
         return cls.query.all()
 
     @classmethod
     def find(cls, by_id):
-        """Finds a record by its ID"""
-        logger.info("Processing lookup for id %s ...", by_id)
+        """Finds a record by ID"""
+        logger.info("Finding record with id: %s", by_id)
         return cls.query.get(by_id)
 
 
@@ -80,37 +71,33 @@ class PersistentBase:
 #  A C C O U N T   M O D E L
 ######################################################################
 class Account(db.Model, PersistentBase):
-    """
-    Class that represents an Account
-    """
-
-    app = None
+    """Class that represents an Account"""
 
     # Table Schema
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    email = db.Column(db.String(64))
-    address = db.Column(db.String(256))
-    phone_number = db.Column(db.String(32), nullable=True)  # phone number is optional
-    date_joined = db.Column(db.Date(), nullable=False, default=date.today())
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+    address = db.Column(db.String(256), nullable=False)
+    phone_number = db.Column(db.String(32), nullable=True)
+    date_joined = db.Column(db.Date(), nullable=False, default=date.today)
 
     def __repr__(self):
         return f"<Account {self.name} id=[{self.id}]>"
 
     def serialize(self):
-        """Serializes a Account into a dictionary"""
+        """Serializes an Account into a dictionary"""
         return {
             "id": self.id,
             "name": self.name,
             "email": self.email,
             "address": self.address,
             "phone_number": self.phone_number,
-            "date_joined": self.date_joined.isoformat()
+            "date_joined": self.date_joined.isoformat() if self.date_joined else None,
         }
 
     def deserialize(self, data):
         """
-        Deserializes a Account from a dictionary
+        Deserializes an Account from a dictionary
 
         Args:
             data (dict): A dictionary containing the resource data
@@ -121,25 +108,17 @@ class Account(db.Model, PersistentBase):
             self.address = data["address"]
             self.phone_number = data.get("phone_number")
             date_joined = data.get("date_joined")
-            if date_joined:
-                self.date_joined = date.fromisoformat(date_joined)
-            else:
-                self.date_joined = date.today()
+            self.date_joined = date.fromisoformat(date_joined) if date_joined else date.today()
         except KeyError as error:
             raise DataValidationError("Invalid Account: missing " + error.args[0]) from error
         except TypeError as error:
             raise DataValidationError(
-                "Invalid Account: body of request contained "
-                "bad or no data - " + error.args[0]
+                "Invalid Account: body of request contained bad or no data - " + str(error)
             ) from error
         return self
 
     @classmethod
     def find_by_name(cls, name):
-        """Returns all Accounts with the given name
-
-        Args:
-            name (string): the name of the Accounts you want to match
-        """
-        logger.info("Processing name query for %s ...", name)
-        return cls.query.filter(cls.name == name).all()  # Fixed here
+        """Returns all accounts with the given name"""
+        logger.info("Querying for name: %s", name)
+        return cls.query.filter(cls.name == name).all()
